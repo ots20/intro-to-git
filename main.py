b2d866e1576b2ca52944d2d3c52d6722f6b82b01
Ots <omar.teresa@brainly.com>
2021-05-09 22:48:23 +0200

    Adding new files to feature branch and modified some files from master

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # This is a sample Python script.
 
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
@@ -13,5 +14,6 @@ def print_hi(name):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     print_hi('PyCharm')
+
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
